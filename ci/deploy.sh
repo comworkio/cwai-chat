d0dc88203d90f9f8e30aa9acc6daae934b7f751e
Idriss Neumann <idriss.neumann@comwork.io>
2023-03-25 10:28:56 +0100

    Add matomo

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -7,5 +7,9 @@ ENV_FILE=".env.${ENV}"
 echo "VERSION=${VERSION}" > "${ENV_FILE}"
 echo "API_URL=${API_URL}" >> "${ENV_FILE}"
 
+env|grep -E "^(MATOMO)_"|while read -r; do
+  echo "${REPLY}" >> "${ENV_FILE}"
+done
+
 docker login "${CI_REGISTRY}" --username "${CI_REGISTRY_USER}" --password "${CI_REGISTRY_PASSWORD}"
 docker-compose -f "docker-compose-${ENV}.yml" up -d --force-recreate
